0af58838c648f3a0ab4fca79f2d70a7b63107069
Liminghing <87886235+Liminghing@users.noreply.github.com>
2023-10-08 17:59:36 +0900

    [FEAT] 뷰바인딩

--- Study01/app/src/main/java/com/myqr/study01/MainActivity.kt
+++ Study01/app/src/main/java/com/myqr/study01/MainActivity.kt
@@ -2,11 +2,19 @@ package com.myqr.study01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.myqr.study01.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var b:ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        var i=0
+        b= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(b.root)
+
+        b.tvTest.text="aaaa"
+
+
     }
 }
